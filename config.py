8b2e235d83ef03133676ef47659e0c644be48ed2
joloong <joelloong@gmail.com>
2021-09-05 23:00:25 +0800

    Fix sqlalchemy database uri bug

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import os
 
 class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PORT = int(os.environ.get("PORT", 5000))
