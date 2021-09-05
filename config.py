396baf765d8bb874662c6c7ecbb4df73014eeb62
joloong <joelloong@gmail.com>
2021-09-05 22:55:38 +0800

    Minor updates

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PORT = int(os.environ.get('PORT', 5000))
+    PORT = int(os.environ.get("PORT", 5000))
